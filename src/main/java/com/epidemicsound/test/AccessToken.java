094dfc232b38e5ee74ebc13cbee0416e155023f5
Adrien Brunelat <adrien.brunelat@gmail.com>
2021-07-11 01:58:54 +0200

    Wrote some Spring web test code.

---
+++ src/main/java/com/epidemicsound/test/AccessToken.java
@@ -0,0 +1,25 @@
+package com.epidemicsound.test;
+
+public class AccessToken {
+    private final String access_token;
+    private final String token_type;
+    private final int expires_in;
+
+    public AccessToken(String access_token, String token_type, int expires_in) {
+        this.access_token = access_token;
+        this.token_type = token_type;
+        this.expires_in = expires_in;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public int getExpires_in() {
+        return expires_in;
+    }
+}
--- src/main/java/com/epidemicsound/test/Application.java
+++ src/main/java/com/epidemicsound/test/Application.java
@@ -1,7 +1,41 @@
 package com.epidemicsound.test;
 
-public class Application {
-    public static void main(String[] args) {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class Application {
+    public static final String API_URL = "https://accounts.spotify.com/api";
+    public static final String CLIENT_ID = "1006b92265b6408683db8854959d4e42";
+    public static final String CLIENT_SECRET = "582cadd8604547e1ad9ad7ccf955259c";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) {
+        return args -> {
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("client_id", CLIENT_ID);
+            parameters.put("client_secret", CLIENT_SECRET);
+
+            String body = "grant_type=client_credentials";
+
+            AccessToken token = restTemplate.postForObject(API_URL + "/token", body, AccessToken.class, parameters);
+            System.out.println(token);
+        };
     }
 }
